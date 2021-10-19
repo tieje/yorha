3ce7f29925844a837e579eb1ebc35c7d149b5ae3
Justin Masayda <justin.masayda@gmail.com>
2021-10-19 13:13:09 -0400

    Add user test

--- Backend/accounts/tests.py
+++ Backend/accounts/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestCustomUser(TestCase):
+    """Tests `CustomUser`."""
+
+    def test_create_user(self):
+        """Should create a custom user."""
+        CustomUser = get_user_model()
+        test_user = CustomUser.objects.create_user(
+            username = 'test_username',
+            email = 'test@email.com',
+            password = 'testpass123',
+        )
+
+        self.assertEqual(test_user.username, 'test_username')
+        self.assertEqual(test_user.email, 'test@email.com')
+        self.assertEqual(test_user.location.coords, (0.0, 0.0))
+        self.assertEqual(test_user.gender, 'non-binary')
+        self.assertTrue(test_user.is_active)
+        self.assertFalse(test_user.is_staff)
+        self.assertFalse(test_user.is_superuser)
