2a3481e00ef25d07ef8b36036828f36fddedb5b4
Justin Masayda <justin.masayda@gmail.com>
2021-10-24 22:36:35 -0400

    Update schema to accept arguments

--- Backend/config/schema.py
+++ Backend/config/schema.py
@@ -1,16 +1,27 @@
+"""
+Defines the GraphQL Schema using Graphene.
+"""
+from accounts.models import Archetype, CustomUser
+from django.contrib.auth import get_user_model
 import graphene
 from graphene_django import DjangoObjectType, DjangoListField
-from graphene_gis.converter import gis_converter
-from django.contrib.auth import get_user_model
-from accounts.models import Archetype
 
 
+# Current user model is CustomUser. See 'accounts.models'.
 user_model = get_user_model()
 
 class AccountType(DjangoObjectType):
     class Meta:
         model = user_model
-        fields = ('id', 'username', 'email', 'gender', 'location', 'is_staff', 'is_active', 'date_joined', 'identify_as', 'search_for',)
+        fields = (
+            'id',
+            'gender',
+            'identify_as',
+            'is_staff',
+            'is_active',
+            'search_for',
+            'username',
+        )
 
 
 class ArchetypeType(DjangoObjectType):
@@ -20,13 +31,42 @@ class ArchetypeType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    accounts = DjangoListField(AccountType)
-    archetypes = DjangoListField(ArchetypeType)
     """
-    accounts = graphene.List(AccountType)
+    Defines which fields are available on a root query and their respective
+    arguments.
+    """
 
-    def resolve_accounts(root, info, **kwargs):
-        return Question.objects.all()
-    """
+    accounts = graphene.List(
+        AccountType,
+        # Arguments available:
+        id=graphene.ID(),
+        is_staff=graphene.Boolean(),
+        gender=graphene.String(),
+    )
+    archetypes = DjangoListField(ArchetypeType)
+
+    def resolve_accounts(
+        root, info, id=None, is_staff=None, gender=None):
+        """
+        Returns a queryset and processes arguments to GraphQL queries.
+
+        The prototype could take **kwargs, but then the client user does not get
+        an error message if they pass an invalid argument. Listing valid argume-
+        nts in the prototype is more verbose, but enables feedback.
+        """
+        query_set = CustomUser.objects.all()
+
+        # Make sure values are not None; Falsy values are valid but without
+        # `is not None` the block will not execute
+        if id is not None:
+            query_set = query_set.filter(id=id)
+
+        if is_staff is not None:
+            query_set = query_set.filter(is_staff=is_staff)
+
+        if gender is not None:
+            query_set = query_set.filter(gender=gender)
+
+        return query_set
 
 schema = graphene.Schema(query=Query)
