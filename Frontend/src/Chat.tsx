da9f1e3a790c0ccf568c36bf8d7b524acf180a9b
tieje <toj320@gmail.com>
2021-10-26 14:56:08 -0400

    my opinions. Not sure if this will get accepted or pushed

--- Frontend/src/Chat.tsx
+++ Frontend/src/Chat.tsx
@@ -1,26 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-class Chat extends React.Component<any, any> {
-    constructor(props:string){ 
-        super(props);
-        this.state = {
-            message: ''
-        };
-        this.handleOnChange = this.handleOnChange.bind(this);
-    }
-    handleOnChange = (changeEvent: { target: { value: any; }; }) => {
-        this.setState({
-          message: changeEvent.target.value
-        });
-      }; 
-
-    handleFormSubmit = (formSubmitEvent: { preventDefault: () => void; }) => {
-        formSubmitEvent.preventDefault();
-        console.log('Message sent: ', this.state.message);
-      };
-    
-    ChatStyleContainer = styled.div`
+const ChatStyleContainer = styled.div`
     display: flex;
     background-color: #3E4046;
     color: #FFEDE7;
@@ -30,16 +11,17 @@ class Chat extends React.Component<any, any> {
     padding: 1rem;
     margin: 1rem;
 `;
-    render(): JSX.Element {
-        return ( 
-            <this.ChatStyleContainer>
-               <div className="print-message" {...this.state.message}>
-                <input type="text" value={this.state.message} className="" onChange={this.handleOnChange} />
-                <input type="submit" onClick={this.handleFormSubmit} />
-                </div>
-            </this.ChatStyleContainer>
-        )
-    }
-}
 
-export default Chat;
+export const Chat = () => {
+    const [message, setMessage] = useState('none')
+
+    return (
+        <ChatStyleContainer>
+            <div className="print-message">
+                <div>{message}</div>
+                <input type="text"></input>
+                <button>Submit</button>
+            </div>
+        </ChatStyleContainer>
+    )
+}
