1131e3ab38aaeb46f26852fa89c8e34c216f17de
Nels <nelsfichera@gmail.com>
2021-10-26 15:45:25 -0400

    ADDED | Bulletin-style 'chat' component, fully functions (stateless)

--- Frontend/src/Chat.tsx
+++ Frontend/src/Chat.tsx
@@ -1,43 +1,52 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const ChatStyleContainer = styled.div`
+display: flex;
+background-color: #3E4046;
+color: #FFEDE7;
+font-family: 'Space Mono', monospace;
+flex-grow: 1;
+border: 1px solid black;
+padding: 1rem;
+margin: 1rem;
+`;
 class Chat extends React.Component<any, any> {
     constructor(props:string){ 
         super(props);
         this.state = {
-            message: ''
+            message: '',
+            showMessage: false
         };
-        this.handleOnChange = this.handleOnChange.bind(this);
+        
     }
-    handleOnChange = (changeEvent: { target: { value: any; }; }) => {
+    handleOnChange = (event: { target: { value: any; }; }) => {
         this.setState({
-          message: changeEvent.target.value
+          message: event.target.value
         });
       }; 
 
     handleFormSubmit = (formSubmitEvent: { preventDefault: () => void; }) => {
         formSubmitEvent.preventDefault();
         console.log('Message sent: ', this.state.message);
-      };
-    
-    ChatStyleContainer = styled.div`
-    display: flex;
-    background-color: #3E4046;
-    color: #FFEDE7;
-    font-family: 'Space Mono', monospace;
-    flex-grow: 1;
-    border: 1px solid black;
-    padding: 1rem;
-    margin: 1rem;
-`;
+        this.setState({showMessage: true});
+      }
+      
+      displayMessageHandler = (event: {target: {value: any; };}) => {
+        let updatedMessage = event.target.value;
+        this.setState({ message: updatedMessage });
+         
+      }
+
     render(): JSX.Element {
         return ( 
-            <this.ChatStyleContainer>
-               <div className="print-message" {...this.state.message}>
-                <input type="text" value={this.state.message} className="" onChange={this.handleOnChange} />
+            <ChatStyleContainer>
+               <form>
+                <input type="text" value={this.state.message} onChange={this.handleOnChange} />
                 <input type="submit" onClick={this.handleFormSubmit} />
-                </div>
-            </this.ChatStyleContainer>
+                {this.state.showMessage && <p>{this.state.message}</p>}
+                </form>
+            </ChatStyleContainer>
         )
     }
 }
