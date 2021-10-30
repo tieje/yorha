4506b3d2863944a2b9940b37a67809a7be20412e
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2021-10-30 01:54:26 -0400

    Got the first part of anible done. The rpis are set up. Ansible is set up on my mac. I just need to figure out authentication.

---
+++ rpi-hosting/m1-local-setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Setting up Ansible on an M1 mac with homebrew already installed
+# difference between bin/bash and the above shebang lines
+# https://stackoverflow.com/questions/16365130/what-is-the-difference-between-usr-bin-env-bash-and-usr-bin-bash
+# installing ansible on m1 mac
+# https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-macos
+python -m pip install --user ansible
