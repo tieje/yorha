1261bbf6d5aa44b55b4857c3e73c1aa7990c9886
Nels <nelsfichera@gmail.com>
2021-11-03 10:38:21 -0400

    CLEAN UP | Commented on how the component currently works, bugs that need to be sorted out, ways to move forward

--- Frontend/src/Chat.tsx
+++ Frontend/src/Chat.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+//styles the chat container
 const ChatStyleContainer = styled.div`
 display: flex;
 background-color: #3E4046;
@@ -16,6 +16,8 @@ border-radius: 10px;
 height: 10vh;
 width: 40vw;
 `;
+//creates a chat component which by default has an empty string and a boolean of whether or not show a message
+//types should likely be stronger
 class Chat extends React.Component<any, any> {
     constructor(props:string){ 
         super(props);
@@ -25,18 +27,20 @@ class Chat extends React.Component<any, any> {
         };
         
     }
+    //presents the changes the user made to the string--currently needs to be coordinated with displayMessageHandler
+    //because currently once submit is hit once, any changes made to the string appear (not when the user wants to submit them)
     handleOnChange = (event: { target: { value: any; }; }) => {
         this.setState({
           message: event.target.value
         });
       }; 
-
+      //when the user hits submit, this logs their message for future testing. 
     handleFormSubmit = (formSubmitEvent: { preventDefault: () => void; }) => {
         formSubmitEvent.preventDefault();
         console.log('Message sent: ', this.state.message);
         this.setState({showMessage: true});
       }
-      
+      //toggles the boolean of whether or not to show the message
       displayMessageHandler = (event: {target: {value: any; };}) => {
         let updatedMessage = event.target.value;
         this.setState({ message: updatedMessage });
